d022542499542f3f8af98f5f3c1cc8e75fe91092
elysia <a.elysia@proton.me>
2026-06-04 20:59:50 +0800

    fix: uv build on linux
    Signed-off-by: elysia <a.elysia@proton.me>

--- python/olm_build.py
+++ python/olm_build.py
@@ -16,6 +16,7 @@
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 import os
+import re
 import subprocess
 
 from cffi import FFI
@@ -25,16 +26,31 @@ PATH = os.path.dirname(__file__)
 
 DEVELOP = os.environ.get("DEVELOP")
 
-compile_args = ["-I../include"]
+compile_args = ["-Ilibolm/include"]
+link_args = []
 
 if DEVELOP and DEVELOP.lower() in ["yes", "true", "1"]:
     link_args.append('-Wl,-rpath=../build')
 
-PARENT_DIR = os.path.abspath(os.path.join(PATH, ".."))
-lib_build = subprocess.Popen("make static", shell=True, cwd=PARENT_DIR)                                                                                                                
-lib_build.wait()
-headers_build = subprocess.Popen("make headers", shell=True)
-headers_build.wait()
+# Try to build with cmake first, fall back to GNU make
+try:
+    subprocess.run(
+        ["cmake", ".", "-Bbuild", "-DBUILD_SHARED_LIBS=NO"],
+        cwd="libolm", check=True,
+    )
+    subprocess.run(
+        ["cmake", "--build", "build"],
+        cwd="libolm", check=True,
+    )
+except FileNotFoundError:
+    try:
+        # try "gmake" first because some systems have a non-GNU make
+        # installed as "make"
+        subprocess.run(["gmake", "static"], cwd="libolm", check=True)
+    except FileNotFoundError:
+        # some systems have GNU make installed without the leading "g"
+        # so give that a try (though this may fail if it isn't GNU make)
+        subprocess.run(["make", "static"], cwd="libolm", check=True)
 
 ffibuilder.set_source(
     "_libolm",
@@ -46,22 +62,64 @@ ffibuilder.set_source(
         #include <olm/sas.h>
     """,
     libraries=["olm"],
-    library_dirs=[os.path.join("..", "build")],
+    library_dirs=[os.path.join("libolm", "build")],
     extra_compile_args=compile_args,
+    extra_link_args=link_args,
     source_extension=".cpp", # we need to link the C++ standard library, so use a C++ extension
 )
 
-with open(os.path.join(PATH, "include/olm/error.h")) as f:
-    ffibuilder.cdef(f.read(), override=True)
+# Run C preprocessor on the headers to resolve #ifdef, #define, #include etc.
+# that cffi's cdef parser cannot handle.
+preprocess_input = (
+    '#define OLM_STATIC_DEFINE\n'
+    '#include "olm/olm.h"\n'
+    '#include "olm/pk.h"\n'
+    '#include "olm/sas.h"\n'
+)
 
-with open(os.path.join(PATH, "include/olm/olm.h")) as f:
-    ffibuilder.cdef(f.read(), override=True)
 
-with open(os.path.join(PATH, "include/olm/pk.h")) as f:
-    ffibuilder.cdef(f.read(), override=True)
+def _find_cc():
+    """Find a working C compiler for preprocessing headers."""
+    for cc in ["cc", "gcc", "clang"]:
+        try:
+            subprocess.run([cc, "--version"], capture_output=True, check=True)
+            return cc
+        except (FileNotFoundError, subprocess.CalledProcessError):
+            continue
+    raise RuntimeError("No working C compiler found (tried cc, gcc, clang)")
 
-with open(os.path.join(PATH, "include/olm/sas.h")) as f:
-    ffibuilder.cdef(f.read(), override=True)
+
+def _strip_attributes(text):
+    """Strip GCC/Clang __attribute__((...)) with support for nested parens."""
+    MATCH_LIST=[
+        "__attribute__((__aligned__(__alignof__(long long))))",
+        "__attribute__((__aligned__(__alignof__(long double))))"
+    ]
+    for match in MATCH_LIST:
+        text = text.replace(match, "")
+
+    print(text)
+    return text
+
+
+def _cpp(preprocess_input, include_dir):
+    """Preprocess a C header string and return cleaned declarations.
+
+    Uses the C preprocessor to expand macros and includes, then strips
+    compiler-specific syntax that cffi's pycparser cannot handle.
+    """
+    cc = _find_cc()
+    result = subprocess.run(
+        [cc, "-E", "-P", f"-I{include_dir}", "-"],
+        input=preprocess_input,
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    return _strip_attributes(result.stdout)
+
+
+ffibuilder.cdef(_cpp(preprocess_input, os.path.join(PATH, "include")), override=True)
 
 if __name__ == "__main__":
     ffibuilder.compile(verbose=True)
