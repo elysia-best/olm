66e4b735f3741b6565b7ebcb1a4ff3a4d8de17c4
elysia <a.elysia@proton.me>
2026-06-05 09:40:52 +0800

    fix: add build configuration for compiler selection in setup.py
    Signed-off-by: elysia <a.elysia@proton.me>

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
+import sys
 
 setup(
-    cffi_modules=["olm_build.py:ffibuilder"]
+    cffi_modules=["olm_build.py:ffibuilder"],
+    build={
+        "compiler": "mingw32" if (sys.platform == "win32") else "unix",
+    }
 )
