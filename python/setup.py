36bf127ba72acfd7ead8e4c32835670489619d2e
elysia <a.elysia@proton.me>
2026-06-05 10:04:44 +0800

    fix: implement custom build_ext class for Windows compiler configuration in setup.py
    Signed-off-by: elysia <a.elysia@proton.me>

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
+from setuptools.command.build_ext import build_ext
 import sys
+
+
+class BuildExt(build_ext):
+    def finalize_options(self):
+        build_ext.finalize_options(self)
+        if sys.platform == "win32":
+            self.compiler = "mingw32"
+
 
 setup(
     cffi_modules=["olm_build.py:ffibuilder"],
+    cmdclass={"build_ext": BuildExt},
 )
